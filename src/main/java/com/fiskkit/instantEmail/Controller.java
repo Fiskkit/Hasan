9c7e7e67ed3e129dbf629a41515ccf8c8ea216da
Hasan Diwan <hd1@jsc.d8u.us>
2017-06-02 08:26:31 +0200

    remove MySQLAccess, its never used

--- src/main/java/com/fiskkit/instantEmail/Controller.java
+++ src/main/java/com/fiskkit/instantEmail/Controller.java
@@ -7,38 +7,39 @@ import com.jobhive.sakimonkey.data.request.Message;
 import com.jobhive.sakimonkey.data.request.Message.Recipient;
 import com.jobhive.sakimonkey.data.request.Message.Var;
 import com.jobhive.sakimonkey.data.response.MessageStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@Component
 public class Controller {
+  @Autowired UserRepository repository;
+
   private MandrillAsyncClient mandrillAsyncClient = null; // lazy instantiation
 
-  @Value("${apiKey}")
-  private String apiKey;
-  @RequestMapping("/notify")
+  @RequestMapping(value="/notify", method=RequestMethod.HEAD)
   public Boolean notifyMe(@RequestParam(value="user")String userIdAsString) {
-    try {
-      Integer userId = new Integer(userIdAsString);
-      User user = MySQLAccess.getUser(userId);
-      if (mandrillAsyncClient == null){
-        mandrillAsyncClient = new MandrillAsyncClient(apiKey, null);
+    Long userId = new Long(userIdAsString);
+    User user = repository.findOne(userId);
+    String apiKey = System.getProperty("mandrill.apiKey");
+    if (apiKey != null) {
+      mandrillAsyncClient = new MandrillAsyncClient(apiKey, null);
+    } 
+    Message message = new Message();
+    message.setFromEmail("contact@fiskkit.com");
+    message.setSubject("{{user.first_name}}, {{subject_end}}");
+    String templateName = "instant-wip-template";
+    mandrillAsyncClient.api().messages().sendTemplate(templateName, message, new ObjectResponseCallback<MessageStatus[]>() {
+      @Override
+      public void onSuccess (Result<MessageStatus[]> result) {
+        System.setProperty("instantEmailSuccess", "true");
       }
-      Message message = new Message();
-      message.setFromEmail("contact@fiskkit.com");
-      message.setSubject("{{user.first_name}}, {{subject_end}}");
-      String templateName = "instant-wip-template";
-      mandrillAsyncClient.api().messages().sendTemplate(templateName, message, new ObjectResponseCallback<MessageStatus[]>() {
-        @Override
-        public void onSuccess (Result<MessageStatus[]> result) {
-          System.setProperty("instantEmailSuccess", "true");
-        }
-      });
-    } catch (Throwable catchall) {
-      System.setProperty("instantEmailSuccess", "false");
-    }
+    });
     return System.getProperty("instantEmailSuccess") == "true";
   }
 }
