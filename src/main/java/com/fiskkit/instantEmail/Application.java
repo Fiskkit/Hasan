11a415123b6c879ffb01e82ab8a9378c840b3506
Hasan Diwan <hd1@jsc.d8u.us>
2017-06-02 19:10:19 +0200

    configure com.fiskkit.instantEmail.models for JPA

--- src/main/java/com/fiskkit/instantEmail/Application.java
+++ src/main/java/com/fiskkit/instantEmail/Application.java
@@ -2,8 +2,12 @@ package com.fiskkit.instantEmail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.fiskkit.instantEmail.models")
+@EntityScan("com.fiskkit.instantEmail.models")
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
