45e831556ba511a28412b13069b72818200f2343
Hasan Diwan <hd1@jsc.d8u.us>
2017-06-02 19:34:46 +0200

    JPA-ify EmailPriority

--- src/main/java/com/fiskkit/instantEmail/models/EmailPriority.java
+++ src/main/java/com/fiskkit/instantEmail/models/EmailPriority.java
@@ -2,14 +2,17 @@ package com.fiskkit.instantEmail.models;
 
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 /**
  * Created by joshuaellinger on 4/8/15; updated by hasan on 6/02/17.
  */
 @Entity
 public class EmailPriority {
-    private int userId;
-    private int sendAfter;
+  @Id @GeneratedValue private Integer id;
+    private Integer userId;
+    private Integer sendAfter;
     private LocalDateTime addedOn;
 
     public EmailPriority() { 
@@ -20,19 +23,19 @@ public class EmailPriority {
       return addedOn;
     }
 
-    public int getUserId() {
+    public Integer getUserId() {
         return userId;
     }
 
-    public void setUserId(int userId) {
+    public void setUserId(Integer userId) {
         this.userId = userId;
     }
 
-    public int getSendAfter() {
+    public Integer getSendAfter() {
         return sendAfter;
     }
 
-    public void setSendAfter(int sendAfter) {
+    public void setSendAfter(Integer sendAfter) {
         this.sendAfter = sendAfter;
     }
 }
