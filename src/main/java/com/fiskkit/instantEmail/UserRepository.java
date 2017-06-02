200dc7cd1d26b0f510f5a6b8e4310158f1601d80
Hasan Diwan <hd1@jsc.d8u.us>
2017-06-02 19:33:43 +0200

    interfaces can only extend other interfaces

--- src/main/java/com/fiskkit/instantEmail/UserRepository.java
+++ src/main/java/com/fiskkit/instantEmail/UserRepository.java
@@ -3,5 +3,5 @@ package com.fiskkit.instantEmail;
 import com.fiskkit.instantEmail.models.User;
 import org.springframework.data.repository.CrudRepository;
 
-public class UserRepository extends CrudRepository<User,Integer> {
+public interface UserRepository extends CrudRepository<User,Integer> {
 }
