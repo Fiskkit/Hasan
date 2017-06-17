573ceff8a15a7cbeda78a152666d23fbe22e4da6
Hasan Diwan <hd1@jsc.d8u.us>
2017-06-17 16:19:11 -0700

    activate our custom configuration

--- src/main/java/com/fiskkit/instantEmail/Application.java
+++ src/main/java/com/fiskkit/instantEmail/Application.java
@@ -3,6 +3,7 @@ package com.fiskkit.instantEmail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("com.fiskkit.instantEmail.models")
 public class Application {
   public static void main(String[] args) {
+    new AnnotationConfigApplicationContext(Config.class);
     SpringApplication.run(Application.class, args);
   }
 }
