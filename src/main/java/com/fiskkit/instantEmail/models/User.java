a160344b2a43921bbfec1051a130316579a3ec7a
Hasan Diwan <hd1@jsc.d8u.us>
2017-06-18 11:22:47 -0700

    adjust User model

--- src/main/java/com/fiskkit/instantEmail/models/User.java
+++ src/main/java/com/fiskkit/instantEmail/models/User.java
@@ -1,6 +1,7 @@
 package com.fiskkit.instantEmail.models;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,48 +12,54 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
-	@Id @SequenceGenerator(name="pk_sequence",sequenceName="messagesounds_id_seq", allocationSize=1) @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="pk_sequence") Long id;
-	@Column(nullable=false, unique=true) Integer phpId;
-	@Column BigDecimal balance;
-	
-	public User() {
-		this.balance = BigDecimal.ZERO;
-	}
+	@Id
+	@SequenceGenerator(name = "pk_sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pk_sequence")
+	Long id;
+
+	@Column(nullable = false, unique = true)
+	Integer phpId;
+
+	@Column
+	BigDecimal balance = BigDecimal.ZERO;
+
+	@Column
+	LocalDateTime addedAt = LocalDateTime.now();
 
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	public Integer getPhpUserId() {
+
+	public Integer getPhpId() {
 		return phpId;
 	}
-	
-	public void setPhpUserId(Integer phpUserId) {
-		this.phpId = phpUserId;
+
+	public void setPhpId(Integer phpUserId) {
+		phpId = phpUserId;
 	}
-	
+
 	public BigDecimal getBalance() {
 		return balance;
 	}
-	
+
 	public void setBalance(BigDecimal balance) {
 		this.balance = balance;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((phpId == null) ? 0 : phpId.hashCode());
+		result = (prime * result) + ((phpId == null) ? 0 : phpId.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
@@ -73,6 +80,6 @@ public class User {
 			return false;
 		}
 		return true;
-	} 
-	
+	}
+
 }
