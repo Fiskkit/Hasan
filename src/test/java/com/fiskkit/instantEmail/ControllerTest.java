33b1d587b45601bcaf7d0092165fdd9319060bdf
Hasan Diwan <hd1@jsc.d8u.us>
2017-06-18 11:23:11 -0700

    adjust Test to work properly

--- src/test/java/com/fiskkit/instantEmail/ControllerTest.java
+++ src/test/java/com/fiskkit/instantEmail/ControllerTest.java
@@ -1,7 +1,10 @@
 package com.fiskkit.instantEmail;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
+
+import com.fiskkit.instantEmail.models.User;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,29 +15,34 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.fiskkit.instantEmail.models.User;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 
 public class ControllerTest {
-	@Autowired Controller controller;
+	@Autowired
+	Controller controller;
 
-    @LocalServerPort
-    private int port;
+	@LocalServerPort
+	private int port;
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+	@Autowired
+	private TestRestTemplate restTemplate;
 
-	@Test public void isCreatedProperly() throws Exception {
+	@Test
+	public void isCreatedProperly() throws Exception {
 		assertThat(controller).isNotNull();
 	}
-	
-	@Test public void newUserCreated() {
-		assertThat(this.restTemplate.postForObject("http://localhost:"+port+"/newuser?user=-1", null, User.class).getBalance()).isEqualTo(BigDecimal.ZERO);
+
+	@Test
+	public void importUsers() {
+		assertThat(restTemplate.postForObject("http://localhost:" + port + "/user", null, Boolean.class))
+				.isEqualTo(Boolean.TRUE);
 	}
-	
-	@Test public void updateBalanceTest() {
-		assertThat(this.restTemplate.postForObject("http://localhost:"+port+"/balance?email=fiskkit@mailinator.com&amount=1", null, User.class).getBalance()).isEqualTo(BigDecimal.ONE);
+
+	@Test
+	public void updateBalanceTest() {
+		assertThat(restTemplate
+				.postForObject("http://localhost:" + port + "/balance?user=19&amount=1", null, User.class).getBalance())
+						.isGreaterThanOrEqualTo(BigDecimal.ONE);
 	}
 }
