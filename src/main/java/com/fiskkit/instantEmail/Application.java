ac392032649b336a4e2d187e9de5600976e16625
Hasan Diwan <hd1@jsc.d8u.us>
2017-06-20 00:36:41 -0700

    remove @EntityScan and @EnableJpaRepositories, as they are both included in @SpringBootApplication

--- src/main/java/com/fiskkit/instantEmail/Application.java
+++ src/main/java/com/fiskkit/instantEmail/Application.java
@@ -19,12 +19,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = { "com.fiskkit.instantEmail.models" })
-@EntityScan(basePackages = { "com.fiskkit.instantEmail.models" })
 public class Application {
 	private static Logger logger = LoggerFactory.getLogger("Application");
 
