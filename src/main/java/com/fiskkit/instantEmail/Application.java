7770d93908d28bf78123c4eec45dedcd5683ed0b
Hasan Diwan <hd1@jsc.d8u.us>
2017-06-26 17:29:00 -0700

    write (and expose) a Stanford coreNLP-based sentence tokenizer

--- src/main/java/com/fiskkit/instantEmail/Application.java
+++ src/main/java/com/fiskkit/instantEmail/Application.java
@@ -2,19 +2,15 @@ package com.fiskkit.instantEmail;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	@SuppressWarnings("unused")
 	private static Logger logger = LoggerFactory.getLogger("com.fiskkit.instantEmail.Application");
-
-	@Value("${local.server.port}")
-	static int portNumber;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		logger.error("Application bound to " + portNumber);
 	}
 }
